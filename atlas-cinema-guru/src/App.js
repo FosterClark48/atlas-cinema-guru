25acb722c63548e3237f6a008bebe6b7e8c13bd4
FosterClark48 <fosterclark48@yahoo.com>
2024-03-07 10:11:26 -0600

    Set up a basic authentication flow, checking the token's validity on app startup and conditionally rendering content based on the user's authentication status

--- atlas-cinema-guru/src/App.js
+++ atlas-cinema-guru/src/App.js
@@ -1,9 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Dashboard from './routes/dashboard/Dashboard';
+import Authentication from './routes/auth/Authentication';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState("");
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToke");
+
+    if (accessToken) {
+      axios.post('http://localhost:8000/api/auth/', {}, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      })
+      .then(response => {
+        setIsLoggedIn(true);
+        setUserUsername(response.data.user.username);
+      })
+      .catch(error => {
+        console.log('Authentication error:', error);
+      })
+    }
+  }, []);
+
   return (
     <div className="App">
-      
+      {isLoggedIn ? <Dashboard
+        userUsername={userUsername}
+        setIsLoggedIn={setIsLoggedIn}
+      /> : <Authentication
+        setIsLoggedIn={setIsLoggedIn}
+        userUsername={userUsername}
+      />}
     </div>
   );
 }
