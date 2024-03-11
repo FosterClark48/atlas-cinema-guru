8081ebf91f0ec64ca5d7676e1790a7b7acd2c59c
FosterClark48 <fosterclark48@yahoo.com>
2024-03-11 18:09:02 -0500

    Added option for password visibility

--- atlas-cinema-guru/src/routes/auth/Register.js
+++ atlas-cinema-guru/src/routes/auth/Register.js
@@ -1,9 +1,13 @@
+import React, { useState } from 'react';
 import './auth.css';
 import Button from '../../components/general/Button';
 import Input from '../../components/general/Input';
 import { faUser, faLock, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const Register = ({ username, password, setUsername, setPassword }) => {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const togglePasswordVisibility = () => setIsPasswordVisible(!isPasswordVisible);
 
   return (
     <div className="register">
@@ -23,6 +27,9 @@ const Register = ({ username, password, setUsername, setPassword }) => {
         value={password}
         setValue={setPassword}
         icon={faLock}
+        showPasswordToggle={true}
+        isPasswordVisible={isPasswordVisible}
+        togglePassword={togglePasswordVisibility}
       />
       <Button
         label="Sign Up"
