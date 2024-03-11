b39397833463955232b819a9b230eb0140f9601c
FosterClark48 <fosterclark48@yahoo.com>
2024-03-11 14:16:35 -0500

    Added handleSubmit function to handle submission logic depending on the login or register state

--- atlas-cinema-guru/src/routes/auth/Authentication.js
+++ atlas-cinema-guru/src/routes/auth/Authentication.js
@@ -3,14 +3,35 @@ import Button from '../../components/general/Button';
 import React, { useState } from 'react';
 import Login from './Login';
 import Register from './Register';
+import axios from 'axios';
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
   const [_switch, set_switch] = useState(true);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+  const handleSubmit = async (onSubmit) => {
+    onSubmit.preventDefault();
+
+    const endpoint = _switch ? 'http://localhost:8000/api/auth/login' : 'http://localhost:8000/api/auth/register';
+
+    try {
+      const response = await axios.post(endpoint, { username, password });
+
+      const { accessToken } = response.data;
+
+      if (accessToken) {
+        localStorage.setItem('accessToken', accessToken); // Store the token
+        setUserUsername(username); // Update username
+        setIsLoggedIn(true);;
+      }
+    } catch (error) {
+      console.error('Authentication error:', error);
+    }
+  };
+
   return (
-    <form className="authentication">
+    <form className="authentication" onSubmit={handleSubmit}>
       <div className="toggle-auth">
         <Button
           label="Sign In"
