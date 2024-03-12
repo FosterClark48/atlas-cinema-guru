c68d02005739c58a36388f7da570ee9988183f7e
FosterClark48 <fosterclark48@yahoo.com>
2024-03-12 15:35:26 -0500

    added another div and a p tag

--- atlas-cinema-guru/src/routes/navigation/Header.js
+++ atlas-cinema-guru/src/routes/navigation/Header.js
@@ -11,11 +11,14 @@ function Header({ userUsername, setIsLoggedIn }) {
 
   return (
     <nav className='header-nav'>
-      <img src="https://picsum.photos/100/100" alt="User avatar" />
-      <p>Welcome, {userUsername}</p>
-      <span onClick={logout} className="logout-span">
-        <FontAwesomeIcon icon={faSignOutAlt} /> Logout
-      </span>
+      <p>Cinema Guru</p>
+      <div className="user-options">
+        <img src="https://picsum.photos/100/100" alt="User avatar" />
+        <p>Welcome, {userUsername}</p>
+        <span onClick={logout} className="logout-span">
+          <FontAwesomeIcon icon={faSignOutAlt} /> Logout
+        </span>
+      </div>
     </nav>
   );
 }
