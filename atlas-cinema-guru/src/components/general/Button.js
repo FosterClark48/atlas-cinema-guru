7b29efe6927eed2a2af92744ff35bc3bd7dcd99f
FosterClark48 <fosterclark48@yahoo.com>
2024-03-12 11:47:04 -0500

    added type attribute

--- atlas-cinema-guru/src/components/general/Button.js
+++ atlas-cinema-guru/src/components/general/Button.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './general.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Button({ label, className, onClick, icon }) {
+function Button({ label, className, onClick, icon, type }) {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} onClick={onClick} type={type}>
       {icon && <FontAwesomeIcon icon={icon} />} {label}
     </button>
   );
