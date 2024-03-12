e172833143ae61667590b0e7b04c56a100043d20
FosterClark48 <fosterclark48@yahoo.com>
2024-03-12 17:30:31 -0500

    conditionally rendering the class name depending on if user is logged in or not

--- atlas-cinema-guru/src/App.js
+++ atlas-cinema-guru/src/App.js
@@ -31,7 +31,7 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className={`App ${isLoggedIn ? 'dashboard-view' : 'authentication-view'}`}>
       {isLoggedIn ? <Dashboard
         userUsername={userUsername}
         setIsLoggedIn={setIsLoggedIn}
