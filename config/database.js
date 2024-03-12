bf028650c8e4dc3d16c598d85904a74e5fd49705
FosterClark48 <fosterclark48@yahoo.com>
2024-03-12 12:19:02 -0500

    updated Sequelize import to ES6

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-const Sequelize = require('sequelize').Sequelize;
+import { Sequelize } from 'sequelize';
 const sequelize = new Sequelize(
     process.env.DB_SCHEMA || 'postgres',
     process.env.DB_USER || 'postgres',
@@ -14,4 +14,4 @@ const sequelize = new Sequelize(
     }
 );
 
-module.exports = sequelize
+export default sequelize
