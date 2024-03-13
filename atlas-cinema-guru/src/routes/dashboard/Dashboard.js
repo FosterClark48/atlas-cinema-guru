633de0aa865ac1715ce33a9167c1f5978c6d0f5a
FosterClark48 <fosterclark48@yahoo.com>
2024-03-13 12:47:00 -0500

    Added SideBar component to Dashboard component

--- atlas-cinema-guru/src/routes/dashboard/Dashboard.js
+++ atlas-cinema-guru/src/routes/dashboard/Dashboard.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import Header from '../../components/navigation/Header';
 import './dashboard.css';
+import Header from '../../components/navigation/Header';
+import SideBar from '../../components/navigation/SideBar';
 
 function Dashboard({ userUsername, setIsLoggedIn }) {
 
   return (
     <div className="dashboard">
       <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+      <SideBar />
     </div>
   );
 }
