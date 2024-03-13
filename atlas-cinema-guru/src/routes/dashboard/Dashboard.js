ee86043fd9e1904c35b91b16f17b6bc98d9bc52c
FosterClark48 <fosterclark48@yahoo.com>
2024-03-13 13:02:07 -0500

    Added BrowserRouter to Dashboard component

--- atlas-cinema-guru/src/routes/dashboard/Dashboard.js
+++ atlas-cinema-guru/src/routes/dashboard/Dashboard.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import './dashboard.css';
+import { BrowserRouter } from 'react-router-dom';
 import Header from '../../components/navigation/Header';
 import SideBar from '../../components/navigation/SideBar';
 
 function Dashboard({ userUsername, setIsLoggedIn }) {
 
   return (
-    <div className="dashboard">
-      <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
-      <SideBar />
-    </div>
+    <BrowserRouter>
+      <div className="dashboard">
+        <Header userUsername={userUsername} setIsLoggedIn={setIsLoggedIn} />
+        <SideBar />
+      </div>
+    </BrowserRouter>
   );
 }
 
